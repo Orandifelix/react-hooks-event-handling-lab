d1827ed32c7a9e7cc564fa1e9b49094b240f1941
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-05-17 13:20:48 +0300

    Finished

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,18 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleChange(event) {
+    event.preventDefault();
+    console.log("Entering password...");
+    }
     return (
-        <div></div>
-    )
+      <div>
+        <form>
+          <input type="password" name="password" placeholder="Enter pin..." required onChange={handleChange}/>
+          <button>Enter</button>
+        </form>
+      </div>
+    );
 }
 
 export default Keypad;
