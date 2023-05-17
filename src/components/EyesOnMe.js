141dc86d710e54f69a6ff2df2a968f63cbd06b35
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-05-17 13:29:03 +0300

    Finished

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,38 +1,38 @@
-// Code EyesOnMe Component Here
+// // Code EyesOnMe Component Here
 
-import React from "react";
-
-export default function EyesOnMe() {
-  function handleFocus(event) {
-    const message = event.type === "focus" ? "Good!" : "Hey! Eyes on me!";
-    console.log(message);
-  }
-
-  return (
-    <button onFocus={handleFocus} onBlur={handleFocus}>
-      Eyes on me
-    </button>
-  );
-}
-
-
-
-// import React from 'react';
+// import React from "react";
 
 // export default function EyesOnMe() {
-//   function handleFocus() {
-//     console.log('Good!');
-//   }
-
-//   function handleBlur() {
-//     console.log('Hey! Eyes on me!');
+//   function handleFocus(event) {
+//     const message = event.type === "focus" ? "Good!" : "Hey! Eyes on me!";
+//     console.log(message);
 //   }
 
 //   return (
-//     <button onFocus={handleFocus} onBlur={handleBlur}>
+//     <button onFocus={handleFocus} onBlur={handleFocus}>
 //       Eyes on me
 //     </button>
 //   );
 // }
+
+
+
+import React from 'react';
+
+export default function EyesOnMe() {
+  function handleFocus() {
+    console.log('Good!');
+  }
+
+  function handleBlur() {
+    console.log('Hey! Eyes on me!');
+  }
+
+  return (
+    <button onFocus={handleFocus} onBlur={handleBlur}>
+      Eyes on me
+    </button>
+  );
+}
 
 
